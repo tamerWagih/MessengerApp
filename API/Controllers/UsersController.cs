04a2d601fed0032e370aa0f9082e9a6a030b7ccf
Tamer Wagih <tamer@Cstride.com>
2022-10-04 00:20:27 +0200

    Add basic authorization and authentication.

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -4,32 +4,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-  [ApiController]
-  [Route("api/[controller]")]
-  public class UsersController : ControllerBase
-  {
-    private readonly DataContext _context;
-
-    public UsersController(DataContext context)
+    public class UsersController : BaseApiController
     {
-      _context = context;
-    }
+        private readonly DataContext _context;
 
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
-    {
-      return await _context.Users.ToListAsync();
-    }
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
 
-    [HttpGet("{id}")]
-    public async Task<ActionResult<AppUser>> GetUsers(int id)
-    {
-      return await _context.Users.FindAsync(id);
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [Authorize]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUsers(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
     }
-  }
 }
