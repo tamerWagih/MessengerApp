56ef97fe1a3fcf366d958d70629a6455661943cb
Tamer Wagih <tamer@Cstride.com>
2022-10-20 02:04:16 +0200

    Pagination, sorting and filtering

---
+++ client/src/app/_models/userParams.ts
@@ -0,0 +1,19 @@
+import { User } from './User';
+
+export class UserParams {
+  gender: string;
+  minAge: number;
+  maxAge: number;
+  pageNumber: number;
+  pageSize: number;
+  orderBy: string;
+
+  constructor(user: User) {
+    this.gender = user.gender === 'female' ? 'male' : 'female';
+    this.minAge = 18;
+    this.maxAge = 99;
+    this.pageNumber = 1;
+    this.pageSize = 5;
+    this.orderBy = 'lastActive';
+  }
+}
