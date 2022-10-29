536ee0c2dc03cf625429d117e3201346185e0331
Tamer Wagih <tamer@Cstride.com>
2022-10-29 23:56:19 +0200

    Fix

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,51 +1,39 @@
-using Microsoft.OpenApi.Models;
 using API.Extensions;
 using API.Middleware;
 using API.SignalR;
-
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 namespace API
 {
     public class Startup
     {
         private readonly IConfiguration _config;
-
         public Startup(IConfiguration config)
         {
             _config = config;
-
         }
-
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddApplicationServices(_config);
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-            });
             services.AddCors();
             services.AddIdentityService(_config);
             services.AddSignalR();
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseMiddleware<ExceptionMiddleware>();
-
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseCors(x => x.AllowAnyHeader()
-               .AllowAnyMethod()
-               .AllowCredentials()
-               .WithOrigins("https://localhost:4200", "https://messengerapp-tw.herokuapp.com/"));
-
+                .AllowAnyMethod()
+                .AllowCredentials()
+                .WithOrigins("https://localhost:4200"));
             app.UseAuthentication();
-
             app.UseAuthorization();
 
             app.UseDefaultFiles();
