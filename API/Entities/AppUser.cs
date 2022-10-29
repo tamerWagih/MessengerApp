d1470faed0321deb502691a2bc425a0746a3c171
Tamer Wagih <tamer@Cstride.com>
2022-10-29 23:03:51 +0200

    Switch to postgres

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -4,10 +4,14 @@ namespace API.Entities
 {
     public class AppUser : IdentityUser<int>
     {
+        public AppUser()
+        {
+        }
+
         public DateTime DateOfBirth { get; set; }
         public string KnownAs { get; set; }
-        public DateTime Created { get; set; } = DateTime.Now;
-        public DateTime LastActive { get; set; } = DateTime.Now;
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public DateTime LastActive { get; set; } = DateTime.UtcNow;
         public string Gender { get; set; }
         public string Introduction { get; set; }
         public string LookingFor { get; set; }
@@ -23,6 +27,35 @@ namespace API.Entities
         public ICollection<Message> MessagesReceived { get; set; }
 
         public ICollection<AppUserRole> UserRoles { get; set; }
+        public override int Id { get => base.Id; set => base.Id = value; }
+        public override string UserName { get => base.UserName; set => base.UserName = value; }
+        public override string NormalizedUserName { get => base.NormalizedUserName; set => base.NormalizedUserName = value; }
+        public override string Email { get => base.Email; set => base.Email = value; }
+        public override string NormalizedEmail { get => base.NormalizedEmail; set => base.NormalizedEmail = value; }
+        public override bool EmailConfirmed { get => base.EmailConfirmed; set => base.EmailConfirmed = value; }
+        public override string PasswordHash { get => base.PasswordHash; set => base.PasswordHash = value; }
+        public override string SecurityStamp { get => base.SecurityStamp; set => base.SecurityStamp = value; }
+        public override string ConcurrencyStamp { get => base.ConcurrencyStamp; set => base.ConcurrencyStamp = value; }
+        public override string PhoneNumber { get => base.PhoneNumber; set => base.PhoneNumber = value; }
+        public override bool PhoneNumberConfirmed { get => base.PhoneNumberConfirmed; set => base.PhoneNumberConfirmed = value; }
+        public override bool TwoFactorEnabled { get => base.TwoFactorEnabled; set => base.TwoFactorEnabled = value; }
+        public override DateTimeOffset? LockoutEnd { get => base.LockoutEnd; set => base.LockoutEnd = value; }
+        public override bool LockoutEnabled { get => base.LockoutEnabled; set => base.LockoutEnabled = value; }
+        public override int AccessFailedCount { get => base.AccessFailedCount; set => base.AccessFailedCount = value; }
 
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
 }
